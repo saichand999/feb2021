50bb14776ebabab050c45c68a86e749547989910
saichand <saichand.sapbi@gmail.com>
2021-02-17 17:15:49 +0000

    mathsoperations

---
+++ math.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "lets do match"
+
+x=10
+y=20
+
+
+echo "$x + $y = $((x*y))"
---
+++ simplescript.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "this is a test cript"
+echo "this is nthg to do it as of now"
+
